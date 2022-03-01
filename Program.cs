ba28414a9a4c19e3bf4a5c71e980b1d42986ef62
Khang <truongqkhang@gmail.com>
2022-03-01 15:17:24 +0700

    HEHE

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace laptrinh_net
+{
+    class Program{
+        
+        static void Main(string [] args){
+            Clock c = new Clock();
+            AnalogClock ac = new AnalogClock();
+            DigitalClock dc = new DigitalClock();
+            c.ownSecondChange += new Clock.SecondHandler(ac.AC);
+            c.ownSecondChange += new Clock.SecondHandler(dc.DC);
+            c.Run();
+        }
+    }
+}
